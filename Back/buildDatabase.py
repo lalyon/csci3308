98f8e94de65b37022ec44090c38756731c935036
csciuser <csciuser@Lucass-iMac-Pro.local>
2018-06-26 13:32:36 -0600

    python db creation almost complete. overall sentiment is broken. Joel Courtney

--- Back/buildDatabase.py
+++ Back/buildDatabase.py
@@ -6,9 +6,10 @@ from textblob import TextBlob
 import sys
 import json
 import mysql.connector as mariadb
+import random
 
 # Open database connection
-mariadb_connection = mariadb.connect(user='root', password='', database='csci3308')
+mariadb_connection = mariadb.connect(user='root', password='', database='csci3308', charset="utf8mb4")
 cursor = mariadb_connection.cursor()
 
 # Read in secrets file
@@ -16,17 +17,21 @@ with open("../../secrets.json","r") as f:
     secrets = json.load(f)
 
 # Set up tweepy authorization
-auth = tweepy.OAuthHandler(
+auth = tweepy.AppAuthHandler(
         secrets['twitter']['consumer_key'],
         secrets['twitter']['consumer_secret']
 )
-auth.set_access_token(
-        secrets['twitter']['access_token'],
-        secrets['twitter']['access_token_secret']
-)
+#auth.set_access_token(
+#        secrets['twitter']['access_token'],
+#        secrets['twitter']['access_token_secret']
+#)
 
 # Get twitter api wrapper
-api = tweepy.API(auth)
+api = tweepy.API(
+    auth,
+    wait_on_rate_limit = True,
+    wait_on_rate_limit_notify = True
+)
 
 # Get googlemaps api wrapper
 gmaps = googlemaps.Client(key = secrets['google']['api_key'])
@@ -34,7 +39,11 @@ gmaps = googlemaps.Client(key = secrets['google']['api_key'])
 # Get list of places with trend data available. Trim it to 20 cities in the US
 trendPlaces = api.trends_available()
 trendPlaces = [trend for trend in trendPlaces if trend['countryCode'] == 'US']
-trendPlaces = trendPlaces[:5]
+
+# select random cities with trend data
+max = 10
+nPlaces = max if len(trendPlaces) > max else len(trendPlaces)
+trendPlaces = random.sample(trendPlaces,nPlaces)
 
 # Iterate through each place with data
 for trendPlace in trendPlaces:
@@ -60,10 +69,32 @@ for trendPlace in trendPlaces:
     trend = trends[largestInd]
 
     # Get all tweets in this city about this trend. limit to 30 items
-    tweets = tweepy.Cursor(api.search, q=trend['query'], geocode=geocode).items(30)
+    tweets = tweepy.Cursor(api.search, q=trend['query'], geocode=geocode).items(99)
+
+    # Analyze tweet sentiment
+    mostPositive = [(-2, '')] * 5
+    mostNegative = [(2, '')] * 5
+    for tweet in tweets:
+        text = tweet.text
+        sent = TextBlob(text).sentiment.polarity
+        inserted = False
+        for i,p in enumerate(mostPositive):
+            if p[0] < sent:
+                mostPositive.insert(i, (sent, text))
+                mostPositive.pop()
+                inserted = True
+                break
+        if inserted:
+            continue
+        for i,n in enumerate(mostNegative):
+            if n[0] > sent:
+                mostNegative.insert(i, (sent, text))
+                mostNegative.pop()
+                break
 
     # Join all those tweets into a single newline delimited string
     tweetTexts = "\n".join([tweet.text for tweet in tweets])
+    print(tweetTexts)
 
     # Magic
     blob = TextBlob(tweetTexts)
@@ -72,7 +103,7 @@ for trendPlace in trendPlaces:
     print(trend['name'] + " in " + city + " | Analysis: " + str(blob.sentiment))
 	
     # insert stuff into database
-    cursor.execute("INSERT INTO tweeties (City,Trend,Lat,Lng,Sentiment) VALUES (%s,%s,%s,%s,%s)",(city,trend['name'],str(location['lat']),str(location['lng']),str(blob.sentiment.polarity)))
+    cursor.execute("INSERT INTO tweeties (City,Trend,Lat,Lng,Sentiment,PTweet1,PTweet2,PTweet3,PTweet4,PTweet5,NTweet1,NTweet2,NTweet3,NTweet4,NTweet5) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(city,trend['name'],str(location['lat']),str(location['lng']),str(blob.sentiment.polarity),mostPositive[0][1],mostPositive[1][1],mostPositive[2][1],mostPositive[3][1],mostPositive[4][1],mostNegative[0][1],mostNegative[1][1],mostNegative[2][1],mostNegative[3][1],mostNegative[4][1]))
 
 mariadb_connection.commit()
 mariadb_connection.close()
