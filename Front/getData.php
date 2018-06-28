eb0e8e9381faa55384f9c05bd130aff186d0945e
Grant Copple <grantcopple@cu-engr2-2-1-10.201.66.128.int.colorado.edu>
2018-06-28 13:01:45 -0600

    Here is the most updated version of our php Grant Copple

--- Front/getData.php
+++ Front/getData.php
@@ -1,4 +1,29 @@
 <?php
+#This is a class that we will keep our information in;
+class City {
+  public $Name = "";
+  public $Trend = "";
+  public $Lat = "";
+  public $Lng = "";
+  public $Sentiment = "";
+  public $PTweet1 = "";
+  public $PTweet2 = "";
+  public $PTweet3 = "";
+  public $PTweet4 = "";
+  public $PTweet5 = "";
+  public $NTweet1 = "";
+  public $NTweet2 = "";
+  public $NTweet3 = "";
+  public $NTweet4 = "";
+  public $NTweet5 = "";
+}
+
+#This is a class to put our city class into
+class TimeWindow {
+  public $Time = "";
+  public $Cities = array();
+}
+
 #This stores the server information
 $serverName = "localhost";
 $userName = "root";
@@ -8,7 +33,7 @@ $database = "csci3308";
 #This creates a connection
 $connection = new mysqli($serverName, $userName, $password, $database);
 
-// Check connection
+#Check connection
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
@@ -22,9 +47,51 @@ $endTime = $_GET["endTime"];
 $mariadbData = "SELECT * FROM Tweeties WHERE '" . startTime . "' <= Timestamp AND Timestamp <= '" . endTime "';";
 $tweetData = $connection->query($mariadbData);
 
-if ($tweetData->num_rows > 0) {
-	while($row = $tweetData->fetch_assoc()) {
+#This will make an array that contains the times of each tweet and some.
+$timeArray =  array();
+$dateInterval = new DateInterval('P15M');
+$timeCurser = strtotime($startTime);
+$timeCurser->add($dateInterval);
+if ($tweetData->num_rows > 0){
+  $cities = array();
+  while($row = $tweetData->fetch_assoc()) {
+    if(strtotime($row["Timestamp"]) >  $timeCurser) {
+      $timeWindow = new TimeWindow;
+      $timeWindow->$Time = date_format($timeCurser, 'Y-m-d H:i:s');
+      $timeWindow->$Cities = $cities;
+      $cities = array();
+      $timeCurser->add($dateInterval);
+      array_push($timeArray, $timeWindow);
+    }
+    $city = new City;
+    $city->$Name = $row["Name"];
+    $city->$Trend = $row["Trend"];
+    $city->$Lat = $row["Lat"];
+    $city->$Lng = $row["Lng"];
+    $city->$Sentiment = $row["Sentiment"];
+    $city->$PTweet1 = $row["PTweet1"];
+    $city->$PTweet2 = $row["PTweet2"];
+    $city->$PTweet3 = $row["PTweet3"];
+    $city->$PTweet4 = $row["PTweet4"];
+    $city->$PTweet5 = $row["PTweet5"];
+    $city->$NTweet1 = $row["PTweet1"];
+    $city->$NTweet2 = $row["PTweet2"];
+    $city->$NTweet3 = $row["PTweet3"];
+    $city->$NTweet4 = $row["PTweet4"];
+    $city->$NTweet5 = $row["NTweet5"];
+    array_push($cities, $city);
+  }
+  $timeWindow = new TimeWindow;
+  $timeWindow->$Time = date_format($timeCurser, 'Y-m-d H:i:s');
+  $timeWindow->$Cities = $cities;
+  array_push($timeArray, $timeWindow);
+}
 
+
+}
+$jsonData = json_encode($timeArray);
+
+echo $jsonData;
 
 $connection->close();
 
