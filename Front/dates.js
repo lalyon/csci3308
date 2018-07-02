316751039a021fccf0c5223347c3843d3e46b3ba
Joel Courtney <joel.courtney@colorado.edu>
2018-07-02 15:33:13 -0600

    called update map on ajax return

--- Front/dates.js
+++ Front/dates.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-   // Bind submit button onclick handler to send an Ajax request and
-   //  process Ajax response.
-   $(':submit').click(function (event) {
-      event.preventDefault();  // Do not run the default action
-      var startTime = ($(':text[name="startTimeMessage"]').val()).replace(' ','%20');
-      console.log(startTime)
-      $("#message").load("getData.php?startTime="+startTime);
-   });
+	// Bind submit button onclick handler to send an Ajax request and
+	//  process Ajax response.
+	$(':submit').click(function (event) {
+		event.preventDefault();  // Do not run the default action
+		var startTime = ($(':text[name="startTimeMessage"]').val()).replace(' ','%20');
+		console.log(startTime)
+		$("#message").load("getData.php?startTime="+startTime);
+		updateMap($('#message').text())
+	});
 });
 
