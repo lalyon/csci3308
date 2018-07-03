ccf818a17e459395b0c14e93fff0e062e6e1e642
Grant Copple <grantcopple@cu-engr2-2-1-10.201.66.240.int.colorado.edu>
2018-07-03 12:20:53 -0600

    The first php test Grant Copple

--- Tests/PhpUnitTests.php
+++ Tests/PhpUnitTests.php
@@ -3,7 +3,11 @@
 
   final class getDataTest extends TestCase {
     public function testGetData(): void {
-      
+      $startTime = "2018-07-01 06:20:15";
+      $endTime = "2018-07-01 06:45:15";
+      $timeWindow = getData($startTime,$endTime);
+      $this->assertNotEmpty($timeWindow);
+      $this->assertNotEquals($timeWindow,"Empty Set returned<br>");
     }
 
   }
