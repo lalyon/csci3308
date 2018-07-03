433486b4b735e8342379e11cc513d3e5fea9bf13
csciuser <csciuser@Lucass-iMac-Pro.local>
2018-07-03 13:27:43 -0600

    php unit tests complete? joel courtney

--- Tests/PhpUnitTests.php
+++ Tests/PhpUnitTests.php
@@ -19,23 +19,25 @@
       $startTime = "2018-06-29 06:20:15";
       $endTime = "2018-07-01 06:20:15";
       $tweetWindow = getData($startTime,$endTime);
-      $jsonTweets = json_decode($tweetWindow, true);
+      $jsonTweets = json_decode($tweetWindow);
+      echo gettype($jsonTweets[0]);
+var_dump($jsonTweets[0]);
       for ($i = 0; $i < count($jsonTweets); $i++) {
-	      $this->assertNotNull($jsonTweets[i]->{'Name'});
-	      $this->assertNotNull($jsonTweets[i]->{'Trend'});
-	      $this->assertNotNull($jsonTweets[i]->{'Lat'});
-	      $this->assertNotNull($jsonTweets[i]->{'Lng'});
-	      $this->assertNotNull($jsonTweets[i]->{'Sentiment'});
-	      $this->assertNotNull($jsonTweets[i]->{'PTweet1'});
-	      $this->assertNotNull($jsonTweets[i]->{'PTweet2'});
-	      $this->assertNotNull($jsonTweets[i]->{'PTweet3'});
-	      $this->assertNotNull($jsonTweets[i]->{'PTweet4'});
-	      $this->assertNotNull($jsonTweets[i]->{'PTweet5'});
-	      $this->assertNotNull($jsonTweets[i]->{'NTweet1'});
-	      $this->assertNotNull($jsonTweets[i]->{'NTweet2'});
-	      $this->assertNotNull($jsonTweets[i]->{'NTweet3'});
-	      $this->assertNotNull($jsonTweets[i]->{'NTweet4'});
-	      $this->assertNotNull($jsonTweets[i]->{'NTweet5'});
+	      $this->assertNotNull($jsonTweets[$i]->City);
+	      $this->assertNotNull($jsonTweets[$i]->Trend);
+	      $this->assertNotNull($jsonTweets[$i]->Lat);
+	      $this->assertNotNull($jsonTweets[$i]->Lng);
+	      $this->assertNotNull($jsonTweets[$i]->Sentiment);
+	      $this->assertNotNull($jsonTweets[$i]->PTweet1);
+	      $this->assertNotNull($jsonTweets[$i]->PTweet2);
+	      $this->assertNotNull($jsonTweets[$i]->PTweet3);
+	      $this->assertNotNull($jsonTweets[$i]->PTweet4);
+	      $this->assertNotNull($jsonTweets[$i]->PTweet5);
+	      $this->assertNotNull($jsonTweets[$i]->NTweet1);
+	      $this->assertNotNull($jsonTweets[$i]->NTweet2);
+	      $this->assertNotNull($jsonTweets[$i]->NTweet3);
+	      $this->assertNotNull($jsonTweets[$i]->NTweet4);
+	      $this->assertNotNull($jsonTweets[$i]->NTweet5);
       }
       $this->assertGreaterThan(15, count($jsonTweets));
     }
