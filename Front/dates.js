5370ed98ce33a45351b6a9d764d7f6e486caa2b4
Joel Courtney <joel.courtney@colorado.edu>
2018-07-03 22:18:21 -0600

    stylizing button

--- Front/dates.js
+++ Front/dates.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	// Bind submit button onclick handler to send an Ajax request and
 	//  process Ajax response.
-	$(':submit').click(function (event) {
+	$('#timeButton').click(function (event) {
 		event.preventDefault();  // Do not run the default action
 		var startTime = ($(':text[name="startTimeMessage"]').val()).replace(' ','%20');
 		console.log(startTime)
